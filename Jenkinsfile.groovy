034968cb2b91f09862e4d37b1531f44e6e0846bb
Atul Sharma <44630029+Atply234@users.noreply.github.com>
2020-11-02 11:37:02 -0600

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,22 @@
+node {
+    
+    checkout scm
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing....'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
